bb7e915788e030ba6513a59f4d3621cfad7fa790
[Mixtrix] <[jefferson024@hotmail.com]>
2026-07-14 22:36:08 -0500

    implementacion de test get gifs by query

--- src/gifs/actions/get-gifs-by-query.actions.test.ts
+++ src/gifs/actions/get-gifs-by-query.actions.test.ts
@@ -1,9 +1,20 @@
-import { describe, expect, test } from "vitest";
+import { beforeEach, describe, expect, test, vi } from "vitest";
 import { getgifsbyquery } from './get-gifs-by-query.actions';
+import AxiosMockadapter from  'axios-mock-adapter'
+import { giphyapi } from "../api/giphy.api";
+import { giphysearchresponsemock } from "../../test/mocks/giphy.response.data.ts";
 
 describe('getgifsbyquery', () => {
 
-    test('should return a list of gifs',async() => {
+    let mock = new AxiosMockadapter(giphyapi)
+
+    beforeEach(() => {
+        //mock.reset
+
+        mock = new AxiosMockadapter(giphyapi)
+    })
+
+  /*  test('should return a list of gifs',async() => {
 
         const gifs = await getgifsbyquery('goku');
         const [gifs1] = gifs
@@ -17,6 +28,65 @@ describe('getgifsbyquery', () => {
             title: expect.any(String),
             url: expect.any(String)
         })
-    })
+    })*/
+
+        test('should return a list of gifs', async() => {
+
+            mock.onGet('/search').reply(200,giphysearchresponsemock)
+
+            const gifs = await getgifsbyquery('metro')
+
+            expect(gifs.length).toBe(10)
+            
+            gifs.forEach(gif => {
+                expect(typeof gif.id).toBe(String)
+                expect(typeof gif.title).toBe(String)
+                expect(typeof gif.url).toBe(String)
+                expect(typeof gif.height).toBe(Number)
+                expect(typeof gif.width).toBe(Number)
+            })
+
+            
+
+    
+    
+
 
 })
+test ('should return an empty list of gifs is query is empty', async () => {
+
+    mock.restore
+
+    const gifs = await getgifsbyquery('');
+    console.log(gifs)
+
+    expect(gifs.length).toBe(0)
+
+            })
+
+            test ('should handle a error when the API returns an error', async () => {
+
+                const consoleErrorSpy = vi.spyOn(console,'error').mockImplementation(()=>{
+                    
+                })
+
+    mock.onGet('/search').reply(400, {data:{
+        message: 'bad request'
+    }})
+
+    const gifs = await getgifsbyquery('goku')
+
+    console.log(gifs)
+
+    expect(gifs.length).toBe(0)
+    expect (consoleErrorSpy).toHaveBeenCalledTimes(1)
+    expect (consoleErrorSpy).toHaveBeenCalledWith(expect.any)
+
+
+
+})
+
+})
+
+
+
